daab5cf70020a86e9c556dbe470467dcb67044c5
Nithis Selvam <nithis.selvam@zucisystems.com>
2023-03-16 22:01:08 +0530

    myFirstCommit

--- Assessment.js
+++ Assessment.js
@@ -1,47 +1,41 @@
 function formValidation() {
-    var fname = document.validation_form.firstName;
-    var email = document.validation_form.email;
-    var number = document.validation_form.number;
-    var gender = document.validation_form.gender;
-    var genderValue = gender.value;
-    var hobbies = document.validation_form.Hobbies;
-    var hobbiesValue = hobbies.value;
-    var country = document.validation_form.country;
+  var fname = document.validation_form.firstName;
+  var email = document.validation_form.email;
+  var number = document.validation_form.number;
+  var gender = document.validation_form.gender;
+  var genderValue = gender.value;
+ // var hobbies = document.validation_form.hobbies.checked;
+  var userCountry = document.getElementById("country");
 
+  // var userCountryValue = userCountry.option.value;
 
-    //file format validtion
-    var fileUpload = document.reg_form.fileUpload;
-    var filepath = fileUpload.value;
-    var fileextension = /\w+\.pdf/g;
-    var file = filepath.match(fileextension);
-    
-    if (fname.value.length <= 0) {
-      document.getElementById("fname").innerHTML = "Please Enter Your Name";
+  if (fname.value.length <= 0) {
+    document.getElementById("fname").innerHTML = "Please Enter Your Name";
+
+  }
+  if (email.value.length <= 0) {
+    document.getElementById("email").innerHTML = "Please Enter Your Email Address";
+
+  }
+   if (number.value.length <= 0) {
+      document.getElementById("number").innerHTML = "Enter Valid data";
+
+    }
+  if(genderValue.length <= 0){
+      document.getElementById("gender").innerHTML = "Enter Valid data";
+  
+  }
+  if(validation_form.hobbies[0].checked==false && validation_form.hobbies[1].checked==false && validation_form.hobbies[2].checked==false ){
+      document.getElementById("hobbiesValidation").innerHTML = "Enter Valid data";
      
-    }
-    if (email.value.length <= 0) {
-      document.getElementById("email").innerHTML = "Please Enter Your Email Address";
-    
-    }
-     if (number.value.length <= 0) {
-        document.getElementById("number").innerHTML = "Enter Valida";
-       
-      }
-    if(genderValue.length <= 0){
-        document.getElementById("gender").innerHTML = "Enter Valid data";
-    return false;
-    }
-    if(hobbiesValue.length <= 0){
-        document.getElementById("hobbies").innerHTML = "Enter Valid data";
-      console.log(hobbiesValue);
-        return false;
-    }
-    if(country.value.length <=0){
-        document.getElementById("country").innerHTML = "Select the country";
-
-        return false;
-    }
-return true;
   }
 
- 
+  if (userCountry.value == "") {
+    document.getElementById("countryValidation").innerHTML =
+      "Select the country";
+    // alert("Enter")
+   
+    return false;
+  }
+  return true;
+}
